2ee791510257955073a2a71da7f2fe4a4ef94acc
Wojciech Jaroński <wojtasj1996@gmail.com>
2018-05-25 13:39:10 +0200

    Update definitions.h

--- definitions.h
+++ definitions.h
@@ -3,7 +3,9 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 #include <pthread.h>
+#include <math.h>
 
 #define MSG_SIZE 3
 #define MSG_TAG_REQUEST 100
@@ -15,15 +17,22 @@
 
 #define MAX_IN_ROOM 2
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KGRNB  "\x1B[32m"
-#define KYEL  "\x1B[33m"
+#define KNRM   "\x1B[0m"
+#define KRED   "\x1B[31m"
+#define KREDB  "\x1B[91m"
+#define RED  "\x1B[91m"
+#define KGRN   "\x1B[32m"
+#define KGRNB  "\x1B[92m"
+#define GR  "\x1B[92m"
+#define KYEL   "\x1B[33m"
 #define KYELB  "\x1B[93m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
+#define Y  "\x1B[93m"
+#define KBLU   "\x1B[34m"
+#define KBLUB  "\x1B[94m"
+#define BL  "\x1B[94m"
+#define KMAG   "\x1B[35m"
 #define KMAGB  "\x1B[95m"
-#define KCYN  "\x1B[36m"
+#define KCYN   "\x1B[36m"
 #define KCYNB  "\x1B[96m"
-#define KWHT  "\x1B[37m"
+#define C  "\x1B[96m"
+#define KWHT   "\x1B[37m"
